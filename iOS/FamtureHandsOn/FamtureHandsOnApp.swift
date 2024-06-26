8c9b0dc28a93c9aee152d4eb52bfbedd6cec6e54
Yabanin <yabanin@icloud.com>
2024-06-27 01:39:11 +0900

    Add iOS project

---
+++ iOS/FamtureHandsOn/FamtureHandsOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamtureHandsOnApp.swift
+//  FamtureHandsOn
+//
+//  Created by Ryo Hanma on 2024/06/27.
+//
+
+import SwiftUI
+
+@main
+struct FamtureHandsOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
