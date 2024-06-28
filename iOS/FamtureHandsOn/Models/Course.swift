469fbe2e1474dc7e04081460d843fd4d7eecec6e
Yabanin <yabanin@icloud.com>
2024-06-29 03:14:06 +0900

    Add data model

---
+++ iOS/FamtureHandsOn/Models/Course.swift
@@ -0,0 +1,13 @@
+//
+//  Course.swift
+//  FamtureHandsOn
+//
+//  Created by Ryo Hanma on 2024/06/29.
+//
+
+import Foundation
+
+struct Course: Decodable {
+    let id: Int
+    let title: String
+}
---
+++ iOS/FamtureHandsOn/Models/Material.swift
@@ -0,0 +1,13 @@
+//
+//  Material.swift
+//  FamtureHandsOn
+//
+//  Created by Ryo Hanma on 2024/06/29.
+//
+
+import Foundation
+
+struct Material: Decodable {
+    let title: String
+    let content: String
+}
